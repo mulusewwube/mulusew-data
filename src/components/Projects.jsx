85697c12235165a87de7e661c2c5f5fd65e467d3
Mulusew Wube <158047600+mulusewwube@users.noreply.github.com>
2024-12-27 09:59:50 -0800

    Update Projects.jsx
    project link

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,7 +7,7 @@ function Projects() {
       title: "Optima",
       description: "Combines habit tracking and project management.",
       image: "/assets/images/data1.png",
-      link: "https://www.upwork.com/nx/find-work/best-matches",
+      link: "#",
     },
     {
       title: "Union",
